8852ae9ff69c82ed23b5edc0782e7d69a9f985a9
hitalodev <hitalohpa@gmail.com>
2024-02-02 00:22:05 -0300

    integração com a api da Open AI

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -4,27 +4,15 @@ import { Link } from 'react-router-dom';
 import { useState } from 'react';
 
 function Cards() {
-
-  const [itemClicado, setItemClicado] = useState(null)
-
-  const armazenarInfo = (item) => {
-
-    setItemClicado(item)
-
-  }
-
   return (
     <>
       <div className="cards">
       {data.map((item) => (
         <Link to={`/rooms/${item.id}`} className='link' key={item.id}>
-            <div className='card' key={item.id} onClick={() => armazenarInfo(item)}>
+            <div className='card' key={item.id}>
               <img 
-              src={item.images[0]} 
-              
+              src={item.images[0]}          
               className='card-img'/>
-
-
               <h3 className='card-cidade'>{item.city}</h3>
               <p className='card-pais'>{item.country}</p>
               <p className='card-data'>{item.startDate} - {item.endDate}</p>
--- src/components/Rooms/Rooms.jsx
+++ src/components/Rooms/Rooms.jsx
@@ -1,14 +1,81 @@
-import Header from "../Header/Header.jsx"
-import { useLocation } from "react-router-dom"
-import { data } from "../../data.jsx"
+import OpenAI from "openai";
+import React, { useEffect, useState } from "react";
+import Header from "../Header/Header.jsx";
+import { useLocation, useParams } from "react-router-dom";
+import { data } from "../../data.jsx";
+import "./rooms.css";
+
+const openai = new OpenAI({
+  apiKey: process.env.REACT_APP_OPENAI_API_KEY,
+  dangerouslyAllowBrowser: true
+});
+
+const response = await openai.chat.completions.create({
+  model: "gpt-3.5-turbo",
+  messages: [
+    {
+      role: "system",
+      content: "",
+    },
+    {
+      role: "user",
+      content: "Qual é a capital do Brasil?",
+    },
+  ],
+  temperature: 0.7,
+  max_tokens: 64,
+  top_p: 1,
+});
+
+console.log(response);
 
 function Rooms() {
+  const [itemClicado, setItemClicado] = useState();
+  const { id } = useParams();
+
+  useEffect(() => {
+    setItemClicado(data.find((item) => item.id === id));
+  }, [id]);
 
   return (
-    <div>
+    <div className="room-container">
       <Header />
+
+      <div className="texto-central">
+        <h1>{`${itemClicado?.country.toUpperCase()} - ${itemClicado?.city.toUpperCase()}`}</h1>
+      </div>
+
+      <div className="imagens">
+        <div className="img-left">
+          <img src={itemClicado?.images[0]} alt="" />
+        </div>
+
+        <div className="imgs-right">
+          <div className="imgs-top">
+            <img src={itemClicado?.images[0]} alt="" />
+            <img src={itemClicado?.images[1]} alt="" />
+          </div>
+
+          <div className="imgs-bottom">
+            <img src={itemClicado?.images[2]} alt="" />
+            <img src={itemClicado?.images[3]} alt="" />
+          </div>
+        </div>
+      </div>
+
+      <div className="response">
+        <div className="button">
+          <button>Gerar plano de viagem</button>
+        </div>
+
+        <div className="box">
+          <div className="result">
+            <p>teste</p>
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Rooms
+export default Rooms;
